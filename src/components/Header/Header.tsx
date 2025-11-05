83cdb4b3d3755f85d2013025e4b734021089d328
corechuv <chuv.ole@icloud.com>
2025-11-05 17:25:48 +0100

    k

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -88,7 +88,6 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
                 <nav className={cls.header__nav}>
                     <div className={cls.logo}>
                         <img src="/logo_full.png" alt="logo-mira" className={cls.logo__img} />
-                        MIRA
                         <span className={cls.sl}>devices</span>
                     </div>
                     <div className={cls.f}>
